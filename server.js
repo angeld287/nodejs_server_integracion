e24b68b94f64be7a54c6f977189f184790bb9d5f
Angel Angeles <aangeles@argentuminc.com>
2017-12-07 19:16:00 -0400

    adding body-parser

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+
 app.listen(3000, function() {
     console.log('listening on 3000')
 })
