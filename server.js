c0db9ecc056a630cc3739dc5ee947a9b7179f6be
Angel Angeles <aangeles@argentuminc.com>
2017-12-11 14:47:58 -0400

    adding mongodb

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-
 app.listen(3000, function() {
     console.log('listening on 3000')
 })
